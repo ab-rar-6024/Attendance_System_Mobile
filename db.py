eabf75d51b018f5faa9865c9ea340fb0b9a5cd5d
Mohamed Abrar S A <samohamedabrar2005@gmail.com>
2026-01-19 20:37:55 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import psycopg2
 
 def connect_db():
     return psycopg2.connect(
-        host="db.oebnbvzxgnsqwyhouqvv.supabase.co",
-        database="postgres",
-        user="postgres",
-        password=os.getenv("SUPABASE_PASSWORD", "Abrar 2005#24"),
+        host=os.getenv("SUPABASE_HOST"),
+        database=os.getenv("SUPABASE_DB"),
+        user=os.getenv("SUPABASE_USER"),
+        password=os.getenv("SUPABASE_PASSWORD"),
         port=5432
     )
