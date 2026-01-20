5abe76792fd52e67797097f2ad716fd70b39f1c8
Mohamed Abrar S A <samohamedabrar2005@gmail.com>
2026-01-20 19:20:00 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,6 +4,5 @@ import psycopg2
 def connect_db():
     return psycopg2.connect(
         os.getenv("DATABASE_URL"),
-        sslmode="require",
-        options="-c inet_protocols=ipv4"
+        sslmode="require"
     )
